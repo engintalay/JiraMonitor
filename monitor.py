469b82c087b09dd59ffcdf0b11a1a3daf3262adc
Engin Talay <etalay@gelirler.gov.tr>
2026-03-26 17:25:14 +0300

    Version bump: 1.0.1

--- monitor.py
+++ monitor.py
@@ -5,7 +5,7 @@ Jira Monitor - Bağımsız Masaüstü Uygulaması
 Python + Tkinter ile hazırlanmıştır.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 import tkinter as tk
 from tkinter import ttk, messagebox, scrolledtext

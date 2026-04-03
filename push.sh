7877b9abccac0699313ad0f007dc4529a1b24f8b
Engin Talay <etalay@gelirler.gov.tr>
2026-04-03 09:12:50 +0300

    Düzeltme: push.sh - çoklu timestamp formatı

--- push.sh
+++ push.sh
@@ -4,11 +4,18 @@
 VERSION_FILE=".version"
 VERSION=$(cat "$VERSION_FILE")
 
-# Versiyon numarasını artır (patch version) + tarih saat
-IFS='.' read -r major minor patch <<< "$VERSION"
-patch=$((patch + 1))
+# Versiyon numarasını artır - son part'ı alıp artır
+IFS='.' read -ra PARTS <<< "$VERSION"
+LAST_PART="${PARTS[-1]}"
+# Sayısal kısım
+NUM_PART=$(echo "$LAST_PART" | grep -oE '^[0-9]+')
+if [ -z "$NUM_PART" ]; then
+    NUM_PART=1
+else
+    NUM_PART=$((NUM_PART + 1))
+fi
 TIMESTAMP=$(date +"%Y%m%d%H%M")
-NEW_VERSION="$major.$minor.$patch.$TIMESTAMP"
+NEW_VERSION="1.0.3.$TIMESTAMP$NUM_PART"
 
 # Versiyon dosyasını güncelle
 echo "$NEW_VERSION" > "$VERSION_FILE"
